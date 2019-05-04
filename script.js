da9dbc74dd82418425e066428ccbc795630356ab
Jeremiah <almostgone47@hotmail.com>
2019-05-04 15:23:15 +0800

    add html UI

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let humanWins = 0;
 let compWins = 0;
+const roundPicks = document.getElementById("roundPicks");
 
 const computerPlay = () => {
   return num = (Math.floor(Math.random() * 3) + 1);
@@ -20,42 +21,54 @@ const getResult = (human, computer) => {
     return "You didn't enter a number!!";
   } else if(human == 1 && computer === 3){
     humanWins++;
-    return "You win!!!";
+    return "Point for you!";
   } else if(human == 2 && computer === 1){
     humanWins++;
-    return "You win!!!";
+    return "Point for you!";
   } else if(human == 3 && computer === 2){
     humanWins++;
-    return "You win!!";
+    return "Point for you!";
   } else if(human == computer){
     return "Tie! Play again!!";
   } else {
     compWins++;
-    return "Sorry, you lose.";
+    return "Point to computer. :(";
   }
 }
 
 const checkWinner = () => {
-  console.log("Score is Computer: " + compWins + " Human: " + humanWins);
+  const score = document.getElementById("score");
+
+  score.innerHTML = ("Score is Computer: " + compWins + " Human: " + humanWins);
+
   if (humanWins == 5){
     humanWins = 0;
     compWins = 0;
-    return "You win!!!!"
+    roundPicks.innerHTML = "You win!!!!";
   } else if (compWins == 5){
     humanWins = 0;
     compWins = 0;
-    return "You lose!!"
+    roundPicks.innerHTML = "You lose!!";
   }
 }
 
-const playGame = () => {
-  let human = window.prompt("Enter 1 for Rock, 2 for Paper or 3 for Scissors");
-  let computer = computerPlay();
+const playGame = (humanGuess) => {
+  const human = humanGuess
+  const computer = computerPlay();
+  const roundWinner = document.getElementById("roundWinner");
 
-  console.log(getItem(human) + " vs. " + getItem(computer));
+  roundPicks.innerHTML = (getItem(human) + " vs. " + getItem(computer));
 
   const result = getResult(human, computer);
-  console.log(result);
+  roundWinner.innerHTML = result;
+
   checkWinner();
-  return result;
 }
+
+const buttons = document.querySelectorAll('button');
+
+let human = buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    playGame(button.value);
+  });
+});
